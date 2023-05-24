819fa8031cd702fee394d811bafd40639e9e6e2b
Fabián Ariel <fsosaolea@gmail.com>
2023-05-24 18:55:54 -0300

    minor errors

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import { AppRoutes } from "./routes";
 export const App = () => {
   return (
     <div className="App">
-      <>
         <Layout>
           <AppRoutes />
         </Layout>
-      </>
     </div>
   );
 };
